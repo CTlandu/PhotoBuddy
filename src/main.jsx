7c9df819d783374a3a884acb06ba75eaa550b0bb
CTlandu <jizhoutang@outlook.com>
2024-07-29 22:36:00 -0400

    [Update]安装了auth0-react disable了App.js里的AuthProvider和createStore, 在main.jsx里添加了Auth0Provider.

--- src/main.jsx
+++ src/main.jsx
@@ -3,10 +3,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './index.css';
+import { Auth0Provider } from '@auth0/auth0-react';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <Auth0Provider
+      domain="dev-m5ocddyzhqndhe7x.us.auth0.com"
+      clientId='B2XOe0G0BdbujW0PqBo2ppdINusJW5yq'
+      authorizationParams={{
+        redirect_uri: window.location.origin
+      }}>
+      
+      <App />
+    </Auth0Provider>
   </React.StrictMode>
 );
