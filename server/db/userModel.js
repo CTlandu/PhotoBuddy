8d30b81c2dc3bf562b3db04151482b184cd96b5a
CTlandu <jizhoutang@outlook.com>
2024-08-04 18:39:01 -0400

    [Update] 把supertokenInit全部整合到了单独的supertokensInit.js文件里作为一个方法,使server更加简洁

--- server/db/userModel.js
+++ server/db/userModel.js
@@ -1,7 +1,11 @@
 const mongoose = require("mongoose")
 const UserSchema = new mongoose.Schema({
   email: { type: String, required: [true, "please provide an email!"], unique: [true, "email already exists!"] },
-  password: { type: String, required: [true, "please provide a password"], unique: false },
+  // password: { type: String, required: [true, "please provide a password"], unique: false },
+  id: { type: String, required: [true], unique: true },
+  phoneNumber: {type: String},
+  timeJoined: {type: String},
+  verified: {type: Boolean},
 })
 
 // "create a user table or collection if there is no table with that name already".
