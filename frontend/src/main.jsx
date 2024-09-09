2d0a801c3c5bd69997b9ce2e8c98e73485d2acac
CTlandu <jizhoutang@outlook.com>
2024-09-09 01:54:27 -0400

    [Files Change] 删掉了已经弃用的组件
    顺便更新了一下main.jsx的注释

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -7,6 +7,7 @@ import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
+  // Strict Mode用于开发模式，它会对组件内的副作用和异步请求进行警告。
   <React.StrictMode>
       <App />
   </React.StrictMode>
