805883e002b60a5db98e8e7134bca83cdc5c1217
CTlandu <jizhoutang@outlook.com>
2024-09-12 01:00:22 -0400

    [Minor Fix] Remove "close" button on profileCards

--- frontend/src/components/PhotographerCard.jsx
+++ frontend/src/components/PhotographerCard.jsx
@@ -1,14 +1,11 @@
-import React from 'react';
-import ImageSlider from './ModelImageSlider';
+import React from "react";
+import ImageSlider from "./ModelImageSlider";
 
-
-
-import { useState,useEffect } from 'react';
-import Empty_Avatar from '../assets/empty_avatar.jpg';
-import PhotographerImageSlider from './PhotographerImageSlider';
+import { useState, useEffect } from "react";
+import Empty_Avatar from "../assets/empty_avatar.jpg";
+import PhotographerImageSlider from "./PhotographerImageSlider";
 
 const PhotographerCard = (props) => {
-
   const [profile, setProfile] = useState(props.fetched_profile);
   const [loading, setLoading] = useState(props.isLoading);
 
@@ -16,13 +13,17 @@ const PhotographerCard = (props) => {
 
   const handlePrev = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === 0 ? profile.photographer_info.photographer_images.length - 1 : prevIndex - 1
+      prevIndex === 0
+        ? profile.photographer_info.photographer_images.length - 1
+        : prevIndex - 1
     );
   };
 
   const handleNext = () => {
     setCurrentIndex((prevIndex) =>
-      prevIndex === profile.photographer_info.photographer_images.length - 1 ? 0 : prevIndex + 1
+      prevIndex === profile.photographer_info.photographer_images.length - 1
+        ? 0
+        : prevIndex + 1
     );
   };
 
@@ -32,11 +33,14 @@ const PhotographerCard = (props) => {
     const today = new Date();
     let age = today.getFullYear() - birthDate.getFullYear();
     const monthDiff = today.getMonth() - birthDate.getMonth();
-  
-    if (monthDiff < 0 || (monthDiff === 0 && today.getDate() < birthDate.getDate())) {
+
+    if (
+      monthDiff < 0 ||
+      (monthDiff === 0 && today.getDate() < birthDate.getDate())
+    ) {
       age--;
     }
-  
+
     return age;
   };
 
@@ -45,12 +49,18 @@ const PhotographerCard = (props) => {
       {loading ? null : (
         <div className="card bg-base-100 w-full h-full shadow-xl m-2">
           <figure className="h-96">
-            <img src={profile.photographer_info.photographer_images[0]} alt="Profile" className="w-full h-full object-cover"/>
+            <img
+              src={profile.photographer_info.photographer_images[0]}
+              alt="Profile"
+              className="w-full h-full object-cover"
+            />
           </figure>
           <div className="card-body">
             <button
               className="btn btn-link"
-              onClick={() => document.getElementById(props.modal_index).showModal()}
+              onClick={() =>
+                document.getElementById(props.modal_index).showModal()
+              }
             >
               See profile
             </button>
@@ -62,11 +72,13 @@ const PhotographerCard = (props) => {
             </h2>
             <p className="">Looking for:</p>
             <ul>
-              {profile.photographer_info.photographer_lookingfor.map((string, index) => (
-                <li key={index}>
-                  <div className="badge badge-primary">{string}</div>
-                </li>
-              ))}
+              {profile.photographer_info.photographer_lookingfor.map(
+                (string, index) => (
+                  <li key={index}>
+                    <div className="badge badge-primary">{string}</div>
+                  </li>
+                )
+              )}
             </ul>
             <p>Distance from you:</p>
             <div className="badge badge-info">1.4 miles</div>
@@ -81,116 +93,117 @@ const PhotographerCard = (props) => {
       )}
 
       {/** Profile Modal */}
-      {
-        loading ? null : (
-            <dialog id={props.modal_index} className="modal">
-            <div className="modal-box max-w-lg max-h-[90vh]">
-              <div className="flex items-center space-x-6">
-                {/* 头像部分 */}
-                <div className="avatar">
-                  <div className="w-24 h-24 rounded-full overflow-hidden">
-                    <img
-                      src={profile.avatar || Empty_Avatar}
-                      alt="Avatar"
-                      className="w-full h-full object-cover"
-                    />
-                  </div>
-                </div>
-
-                {/* 个人信息部分 */}
-                <div className="flex-1 bg-gray-200 p-4 rounded-lg">
-                  {/* 名字 */}
-                  <p><strong>Name:</strong> {profile.preferredName}</p>
-                  {/* Pronouns */}
-                  <p><strong>Pronouns:</strong> {profile.pronouns}</p>
-                  {/* Email */}
-                  <p><strong>Email:</strong> {profile.email}</p>
-                  {/* 生日 */}
-                  {profile.birthday ? (
-                    <p><strong>Age:</strong> {calculateAge(profile.birthday)}</p>
-                  ) : null}
-                  {/* 所在城市 */}
-                  <p>{profile.location || 'Williamsburg, VA'}</p>
-                  {/* 电话 */}
-                  {profile.contact.phoneNumber_preferred && <p>{profile.contact.phoneNumber}</p>}
-                  {/* IG */}
-                  {profile.contact.instagram_preferred && 
-                  (
-                    <p> 
-                      <a 
-                        href={profile.contact.instagram} 
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                        className="text-green hover:underline"
-                      >
-                        <strong>Instagram</strong>
-                      </a>
-                    </p>
-                  )}
-                  {/* linkedIn */}
-                  {profile.contact.linkedin_preferred && 
-                  (
-                    <p> 
-                      <a 
-                        href={profile.contact.linkedin} 
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                        className="text-green hover:underline"
-                      >
-                        <strong>LinkedIn</strong>
-                      </a>
-                    </p>
-                  )}
-                  {/* twitter */}
-                  {profile.contact.twitter_preferred && 
-                  (
-                    <p> 
-                      <a 
-                        href={profile.contact.twitter} 
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                        className="text-green hover:underline"
-                      >
-                        <strong>X (Twitter)</strong>
-                      </a>
-                    </p>
-                  )}
-                  {/* facebook */}
-                  {profile.contact.facebook_preferred && 
-                  (
-                    <p> 
-                      <a 
-                        href={profile.contact.facebook} 
-                        target="_blank" 
-                        rel="noopener noreferrer"
-                        className="text-green hover:underline"
-                      >
-                        <strong>Facebook</strong>
-                      </a>
-                    </p>
-                  )}
+      {loading ? null : (
+        <dialog id={props.modal_index} className="modal">
+          <div className="modal-box max-w-lg max-h-[90vh]">
+            <div className="flex items-center space-x-6">
+              {/* 头像部分 */}
+              <div className="avatar">
+                <div className="w-24 h-24 rounded-full overflow-hidden">
+                  <img
+                    src={profile.avatar || Empty_Avatar}
+                    alt="Avatar"
+                    className="w-full h-full object-cover"
+                  />
                 </div>
               </div>
-            <PhotographerImageSlider profile={profile}/>
 
-              {/** Legacy version found in ModelCard.jsx */}
-
-
+              {/* 个人信息部分 */}
+              <div className="flex-1 bg-gray-200 p-4 rounded-lg">
+                {/* 名字 */}
+                <p>
+                  <strong>Name:</strong> {profile.preferredName}
+                </p>
+                {/* Pronouns */}
+                <p>
+                  <strong>Pronouns:</strong> {profile.pronouns}
+                </p>
+                {/* Email */}
+                <p>
+                  <strong>Email:</strong> {profile.email}
+                </p>
+                {/* 生日 */}
+                {profile.birthday ? (
+                  <p>
+                    <strong>Age:</strong> {calculateAge(profile.birthday)}
+                  </p>
+                ) : null}
+                {/* 所在城市 */}
+                <p>{profile.location || "Williamsburg, VA"}</p>
+                {/* 电话 */}
+                {profile.contact.phoneNumber_preferred && (
+                  <p>{profile.contact.phoneNumber}</p>
+                )}
+                {/* IG */}
+                {profile.contact.instagram_preferred && (
+                  <p>
+                    <a
+                      href={profile.contact.instagram}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-green hover:underline"
+                    >
+                      <strong>Instagram</strong>
+                    </a>
+                  </p>
+                )}
+                {/* linkedIn */}
+                {profile.contact.linkedin_preferred && (
+                  <p>
+                    <a
+                      href={profile.contact.linkedin}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-green hover:underline"
+                    >
+                      <strong>LinkedIn</strong>
+                    </a>
+                  </p>
+                )}
+                {/* twitter */}
+                {profile.contact.twitter_preferred && (
+                  <p>
+                    <a
+                      href={profile.contact.twitter}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-green hover:underline"
+                    >
+                      <strong>X (Twitter)</strong>
+                    </a>
+                  </p>
+                )}
+                {/* facebook */}
+                {profile.contact.facebook_preferred && (
+                  <p>
+                    <a
+                      href={profile.contact.facebook}
+                      target="_blank"
+                      rel="noopener noreferrer"
+                      className="text-green hover:underline"
+                    >
+                      <strong>Facebook</strong>
+                    </a>
+                  </p>
+                )}
+              </div>
             </div>
+            <PhotographerImageSlider profile={profile} />
 
-            <form
-              method="dialog"
-              className="modal-backdrop"
-              onClick={() => document.getElementById(props.modal_index).close()}
-            >
-              <button className="btn">Close</button>
-            </form>
-          </dialog>
-        )
-      }
-      
+            {/** Legacy version found in ModelCard.jsx */}
+          </div>
+
+          <form
+            method="dialog"
+            className="modal-backdrop"
+            onClick={() => document.getElementById(props.modal_index).close()}
+          >
+            {/* <button className="btn">Close</button> */}
+          </form>
+        </dialog>
+      )}
     </>
   );
-}
+};
 
 export default PhotographerCard;
