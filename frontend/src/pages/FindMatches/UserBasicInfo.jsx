29771609236039ec3d3cbd706ed591f15dd44f5f
CTlandu <jizhoutang@outlook.com>
2024-10-18 20:56:51 -0400

    [Update] ProfileCard Display Cities now, Modal Style Updates

--- frontend/src/pages/FindMatches/UserBasicInfo.jsx
+++ frontend/src/pages/FindMatches/UserBasicInfo.jsx
@@ -9,32 +9,20 @@ import {
   FaUserCircle,
   FaCamera,
   FaUserTie,
+  FaMapMarkerAlt,
 } from "react-icons/fa";
 
 const UserBasicInfo = ({ profile, calculateAge, role }) => {
   const roleInfo = profile[`${role}_info`] || {};
-  // const experience = roleInfo[`${role}_experience`] || "";
-  // const lookingFor = roleInfo[`${role}_lookingfor`] || [];
-  const biography = roleInfo[`${role}_bio`] || "";
+  const experience = roleInfo[`${role}_experience`] || "";
+  const lookingFor = roleInfo[`${role}_lookingfor`] || [];
 
-  const addressColors = [
-    "bg-gray-200 text-gray-700 dark:bg-gray-700 dark:text-gray-200",
-    "bg-gray-300 text-gray-800 dark:bg-gray-600 dark:text-gray-100",
-    "bg-gray-400 text-gray-900 dark:bg-gray-500 dark:text-gray-50",
-  ];
-
-  // 创建一个函数来生成唯一的地址字符串
-  const getUniqueAddressString = (address) => {
-    const parts = [];
-    if (address.city) parts.push(address.city);
-    if (address.state) parts.push(address.state);
-    if (address.country) parts.push(address.country);
-    return parts.join(", ");
-  };
-
-  // 使用 Set 来去除重复的地址
-  const uniqueAddresses = profile.addresses
-    ? [...new Set(profile.addresses.map(getUniqueAddressString))]
+  // 获取所有地址的城市名称
+  const cities = profile.addresses
+    ? profile.addresses
+        .filter((address) => address && address.formattedCity)
+        .map((address) => address.formattedCity.split(",")[0].trim())
+        .filter((city) => city !== "")
     : [];
 
   const getRoleIcon = (role) => {
@@ -81,7 +69,7 @@ const UserBasicInfo = ({ profile, calculateAge, role }) => {
             <h1 className="text-2xl font-serif italic">
               {profile.preferredName}
             </h1>
-            <span className="bg-blue-100 text-blue-800 text-xs font-medium ml-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300 flex items-center">
+            <span className="bg-blue-100 text-blue-800 text-md font-medium ml-2 px-2.5 py-0.5 rounded-full dark:bg-blue-900 dark:text-blue-300 flex items-center">
               {getRoleIcon(role)}
               {role.charAt(0).toUpperCase() + role.slice(1)}
             </span>
@@ -105,34 +93,7 @@ const UserBasicInfo = ({ profile, calculateAge, role }) => {
         </div>
       </div>
 
-      {uniqueAddresses.length > 0 && (
-        <div className="mt-2 text-center">
-          {uniqueAddresses.map(
-            (addressString, index) =>
-              addressString && (
-                <span
-                  key={index}
-                  className={`inline-block ${
-                    addressColors[index % addressColors.length]
-                  } rounded-full px-3 py-1 text-sm font-semibold mr-2 mb-2`}
-                >
-                  {addressString}
-                </span>
-              )
-          )}
-        </div>
-      )}
-
-      {profile.showEmailOnCard && profile.email && (
-        <a
-          href={`mailto:${profile.email}`}
-          className="text-base flex items-center justify-center mt-2 hover:text-blue-600 transition-colors duration-200"
-        >
-          <FaEnvelope className="mr-2" /> {profile.email}
-        </a>
-      )}
-
-      <div className="flex justify-center space-x-4 mt-4">
+      <div className="flex justify-center space-x-4 mb-2">
         {["instagram", "linkedin", "twitter", "facebook"].map(
           (platform) =>
             profile.contact[`${platform}_preferred`] && (
@@ -153,14 +114,28 @@ const UserBasicInfo = ({ profile, calculateAge, role }) => {
         )}
       </div>
 
-      {biography && (
-        <div className="mt-4 text-center w-full">
-          <h3 className="text-xl font-semibold mb-2">About Me</h3>
-          <p className="italic bg-gray-100 dark:bg-gray-800 p-4 rounded-lg">
-            {biography}
-          </p>
+      {cities.length > 0 && (
+        <div className="flex items-center justify-center mb-2">
+          <FaMapMarkerAlt className="text-red-500 mr-1" />
+          <p>{cities.join(" | ")}</p>
         </div>
       )}
+
+      {profile.showEmailOnCard && profile.email && (
+        <a
+          href={`mailto:${profile.email}`}
+          className="flex items-center justify-center hover:text-blue-600 transition-colors duration-200"
+        >
+          <FaEnvelope className="mr-2" /> {profile.email}
+        </a>
+      )}
+
+      <div className="mt-4 text-center w-full">
+        <h3 className="text-xl font-semibold mb-2">About Me</h3>
+        <p className="italic bg-gray-100 dark:bg-gray-800 p-4 rounded-lg">
+          {roleInfo[`${role}_bio`] || ""}
+        </p>
+      </div>
     </div>
   );
 };
